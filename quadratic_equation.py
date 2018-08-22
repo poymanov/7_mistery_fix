bc8008e65f9b0e087a7c5c393512d05a59ded9b8
Nicolay Poymanov <n.poymanov@gmail.com>
2018-08-22 22:31:05 +0300

    fix pep8 comments

--- quadratic_equation.py
+++ quadratic_equation.py
@@ -4,7 +4,8 @@ from math import sqrt
 def get_roots(a, b, c):
     discriminant = b ** 2 - 4 * a * c
 
-    if discriminant < 0: return None, None
+    if discriminant < 0:
+        return None, None
 
     root1 = (-b - sqrt(discriminant)) / (2 * a)
     root2 = (-b + sqrt(discriminant)) / (2 * a)
